29e68faef11b12526a961ff42877505e387bd2f7
Bhavika Desai <desaibh@gmail.com>
2016-12-16 15:29:59 -0500

    safari window event listener fix

--- xylophone.js
+++ xylophone.js
@@ -10,11 +10,13 @@ function playSound(keyCode) {
 }
 
 function keydownHandler(e) {
+  e = e || window.event
   let keyCodeVal = e.keyCode;
   playSound(keyCodeVal);
 }
 
 function clickHandler(e) {
+  e = e || window.event
   let keyCodeVal;
   if (e.target.className.includes("note")) {
     keyCodeVal = e.toElement.attributes[0].value;
@@ -25,6 +27,7 @@ function clickHandler(e) {
 }
 
 function removeTransform(e) {
+  e = e || window.event
   if (e.propertyName !== 'transform') return;
   this.classList.remove('play');
 }
